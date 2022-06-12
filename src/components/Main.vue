c6d8cbfde7dca072674f7fba339782c5fbfcc9da
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-06-12 12:37:12 +0600

    text editor

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>main page</h1>
+
+    <texteditor />
+
+</template>
+
+<script>
+import texteditor from "../views/text.vue";
+export default {
+    name:"MainPage",
+    components: {
+        texteditor
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/text.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>text</h1>
+    <QuillEditor toolbar="full" />
+    <input type="text" v-model="test" >
+    <button v-on:click="show()">submit</button>
+</template>
+
+<script>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+export default {
+    name: 'MainText',
+    components: {
+        QuillEditor
+    },
+    data() {
+        return {
+        test:'',
+        }
+    },
+    methods:{
+        show(){
+            const datamain = this.test
+            alert(datamain)
+        }
+    }
+}
+</script>
+
+<style>
+</style>
